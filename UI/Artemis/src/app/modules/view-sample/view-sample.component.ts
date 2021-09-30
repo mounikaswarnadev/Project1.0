64500d1bd14f4a13b5e7481bb104334b83146ac7
rajeshd-shell <rajesh.d@shell.com>
2021-09-30 19:10:47 +0530

    add comment field in sample details

--- UI/Artemis/src/app/modules/view-sample/view-sample.component.ts
+++ UI/Artemis/src/app/modules/view-sample/view-sample.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SamplesService } from 'src/app/core/services/samples.service';
+import { ToastService } from 'src/app/core/services/toast/toast.service';
 import { SampleDetails } from 'src/app/models/samples';
 import { Comments, Samples } from 'src/app/models/samples.model';
 
@@ -14,13 +15,16 @@ export class ViewSampleComponent implements OnInit {
   sampleId: string;
   loaded: boolean;
   loading: boolean;
+  commentsDTO: Samples;
+  editableComment: boolean = false;
+  public commentsMaxLength = 450;
   limit = 2;
   size = 2;
   isReadMore = true
   sampleDetails: SampleDetails;
   comments: Comments[];
   content: Comments[];
-  constructor(private route: ActivatedRoute, private samplesService: SamplesService) {
+  constructor(private route: ActivatedRoute, private samplesService: SamplesService, private toastService: ToastService) {
     debugger;
     this.route.queryParams.subscribe(params => { this.sampleId = params.id || ''; });
     this.route.queryParams.subscribe(params => { this.sampleGuid = params.sampleGuid || ''; });
@@ -33,10 +37,26 @@ export class ViewSampleComponent implements OnInit {
 showMore(){
   debugger;
 this.limit = 10;
-let size = this.size + 2;
+let size = this.size + 5;
 this.comments = this.content.slice(0,size)
 this.size = size;
 this.isReadMore = !this.isReadMore;
+}
+showLess(){
+  debugger
+  let size = 5;
+this.comments = this.content.slice(0,size)
+this.size = size;
+}
+openComment(){
+  debugger;
+  this.editableComment = true;
+}
+public closeRefreshPopup(status) {
+  this.editableComment = false;
+  if (status == 'yes') {
+    // this.loadItems();
+  }
 }
   refresh() {
     this.loaded = false;
@@ -49,7 +69,11 @@ this.isReadMore = !this.isReadMore;
       if(data){
         this.sampleDetails = data.samples[0];
         this.content = data.comments;
-        this.size = 2
+        this.size = 5;
+        let a = this.content.length < this.size;
+        if(a){
+          this.loaded = true;
+        }
         this.comments = this.content.slice(0, this.size)
         this.loading = false;
       }
@@ -65,5 +89,30 @@ this.isReadMore = !this.isReadMore;
         //     }
         //   }, (error) => {
         //   });
+      }
+      saveClicked(item) {
+        debugger;
+        let sam = [];
+        this.loading = true;
+        this.commentsDTO = new Samples();
+        this.commentsDTO.ArtemisID = this.sampleDetails.ArtemisID;
+
+            this.commentsDTO.ContactComment = item.value;
+            this.commentsDTO.UpdatedBy = 'Rajesh D';
+            // this.updateDate = new Date();
+            this.commentsDTO.LastUpdatedDate = new Date();
+            this.samplesService.saveSiteComments(this.commentsDTO).subscribe(
+              (args: any) => {
+                this.loading = false;
+                this.closeRefreshPopup('no');
+            this.toastService.success('Comments saved successfully.');
+                // grid.cancelCell();
+                this.refresh();
+              },
+              (error: any) => {
+                this.loading = false;
+                // this.HandleAPIError(error)
+              }
+            )
       }
 }
